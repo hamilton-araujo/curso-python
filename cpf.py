bbdf779cf6addce332077e14c38cb2933ef9a13c
hamilton-araujo <nash260503@gmail.com>
2024-10-21 21:34:17 -0300

    fiz um verificador de cpf :)

--- cpf.py
+++ cpf.py
@@ -32,25 +32,82 @@ while verificador:
 
 cpf_sem_pontos = cpf_digitado.replace(".", "").replace("-", "")
 
-#tratar os dados do cpf para fazer o cálculo do primeiro digito
+# cpf_sem_pontos_sem_digitos = cpf_sem_pontos.replace(cpf_sem_pontos[-2], "").replace(cpf_sem_pontos[-1], "")
+
+# #tratar os dados do cpf para fazer o cálculo do primeiro digito
+# valores_multiplicados = []
+
+# for numero in cpf_sem_pontos_sem_digitos:
+#     multiplicados = (10 - int(numero))*cpf_sem_pontos_sem_digitos[int(numero)]
+#     valores_multiplicados.append(multiplicados)
+
+# soma_multiplicados = np.sum(valores_multiplicados)
+# soma_multiplicados = soma_multiplicados*10
+# #calculando o resto da multiplicação
+
+# resto = soma_multiplicados % 11
+
+# digito1 = 0 if resto > 9 else resto
+
+# if digito1 == cpf_sem_pontos[10]:
+#     print("Primeiro dígito verificado com sucesso!")
+# else:
+#     print("Primeiro dígito inválido!")
+
+# #tratar os dados do cpf para fazer o cálculo do segundo digito
+
+# digito1_str = str(digito1)
+
+# cpf_sem_pontos_sem_digitos1 = cpf_sem_pontos_sem_digitos + digito1_str
+
+# valores_multiplicados1 = []
+
+# for numero in cpf_sem_pontos_sem_digitos1:
+#     multiplicados1 = (11 - numero)*cpf_sem_pontos_sem_digitos1[numero]
+#     valores_multiplicados1.append(multiplicados1)
+
+# soma_multiplicados1 = np.sum(valores_multiplicados1)
+# soma_multiplicados1 = soma_multiplicados1*10
+
+# resto1 = soma_multiplicados1 % 11
+
+# digito2 = 0 if resto1 > 9 else resto1
+
+# if digito2 == cpf_sem_pontos[11]:
+#     print("Segundo dígito verificado com sucesso!")
+# else:
+#     print("Segundo dígito inválido!")
+
+print(cpf_sem_pontos)
+nove_digitos = cpf_sem_pontos[:9]
+
 valores_multiplicados = []
 
-for numero in cpf_sem_pontos:
-    multiplicados = (10 - numero)*cpf_sem_pontos[numero]
+for i in range(len(nove_digitos)):
+    multiplicados = (10 - i)*int(nove_digitos[i])
     valores_multiplicados.append(multiplicados)
 
-soma_multiplicados = np.sum(valores_multiplicados)
-soma_multiplicados = soma_multiplicados*10
-#calculando o resto da multiplicação
-
-resto = soma_multiplicados % 11
+soma = 10*np.sum(valores_multiplicados)
+resto = soma % 11
 
 digito1 = 0 if resto > 9 else resto
+novo_cpf = nove_digitos + str(digito1)
 
-if digito1 == cpf_sem_pontos[10]:
-    print("Primeiro dígito verificado com sucesso!")
+valores_multiplicados1 = []
+
+for i in range(len(novo_cpf)):
+    multiplicados = (11 - i)*int(novo_cpf[i])
+    valores_multiplicados1.append(multiplicados)
+
+soma1 = 10*np.sum(valores_multiplicados1)
+resto1 = soma1 % 11
+
+digito2 = 0 if resto1 > 9 else resto1
+cpf_final = novo_cpf + str(digito2)
+
+print(nove_digitos, digito1, digito2, cpf_sem_pontos, cpf_final)
+
+if cpf_final == cpf_sem_pontos:
+    print("CPF verificado com sucesso!")
 else:
-    print("Primeiro dígito inválido!")
-
-#tratar os dados do cpf para fazer o cálculo do segundo digito
-
+    print("CPF inválido!")
